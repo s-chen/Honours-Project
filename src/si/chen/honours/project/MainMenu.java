ec02e3f51142ff2b4c19fb9ea9a5f9e115cbb497
Si Chen <s-chen@hotmail.co.uk>
2013-09-17 00:05:46 +0100

    Added check to ensure Google Play Services is up to date

--- src/si/chen/honours/project/MainMenu.java
+++ src/si/chen/honours/project/MainMenu.java
@@ -1,22 +1,25 @@
 package si.chen.honours.project;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class MainMenu extends ActionBarActivity {
-
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        
-        
-       
         
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -41,5 +44,21 @@ public class MainMenu extends ActionBarActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	
+    	// Get status code
+    	int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+    	
+    	/* Checks whether Google Play Services APK is up-to-date and direct user to Google Play store
+    	if Google Play services is out of date or missing or to system settings if service is disabled */
+    	if(resultCode != ConnectionResult.SUCCESS) {
+    		Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 1);
+    		dialog.show();
+    		Log.d("GooglePlayVersionCheck", "resultCode: " + resultCode);
+    	} 
     }
 }
